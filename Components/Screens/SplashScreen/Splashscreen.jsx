7a806ac43c12abed359ea9549210e1d96329d96c
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-13 21:58:28 +0500

    Add UI to Splash Screencomponent

--- Components/Screens/SplashScreen/Splashscreen.jsx
+++ Components/Screens/SplashScreen/Splashscreen.jsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, Modal, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import IMS_Logo from "../../Assets/IMS_Logo.png"
 
 const Splashscreen = () => {
   return (
-    <View>
-      <Text>Splashscreen</Text>
+    <Modal animationType='slide'>
+    <View style={styles.container}>
+      <Image style={styles.img} source={IMS_Logo} />
     </View>
+    </Modal>
   )
 }
 
 export default Splashscreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "navy",
+    justifyContent: "center",
+    alignItems: "center" 
+  },
+  img: {
+    width:"60%",
+    height: "30%"
+  }
+})
