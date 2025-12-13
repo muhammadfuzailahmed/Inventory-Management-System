2fba8da2c53c5cc3f846f2b1b405fb875335b203
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-13 18:11:31 +0500

    Add navigation to register and login screen

--- Components/Navigation/Stack/StackScreen.jsx
+++ Components/Navigation/Stack/StackScreen.jsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import login from "../../Authentication/Login/Login"
+import register from "../../Authentication/Register/Register"
 
 const StackScreen = () => {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View>
-      <Text>StackScreen</Text>
-    </View>
+    <Stack.Navigator>
+        <Stack.Screen name='login' component={login}/>
+        <Stack.Screen name='register' component={register}/>
+    </Stack.Navigator>
   )
 }
 
