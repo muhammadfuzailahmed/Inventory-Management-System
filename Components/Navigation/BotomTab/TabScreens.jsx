82d4628f557945270266b80eaa0231a86fe7df13
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-13 14:55:51 +0500

    Setup navigation folder

---
+++ Components/Navigation/BotomTab/TabScreens.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TabScreens = () => {
+  return (
+    <View>
+      <Text>TabScreens</Text>
+    </View>
+  )
+}
+
+export default TabScreens
+
+const styles = StyleSheet.create({})
---
+++ Components/Navigation/Stack/StackScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StackScreen = () => {
+  return (
+    <View>
+      <Text>StackScreen</Text>
+    </View>
+  )
+}
+
+export default StackScreen
+
+const styles = StyleSheet.create({})
