901337a8ad6764acf4eaf2426012d777e6f50e73
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-13 18:42:11 +0500

    Add functionality to button component

--- Components/UI/Button/Button.jsx
+++ Components/UI/Button/Button.jsx
@@ -1,14 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const Button = () => {
+const Button = ({title}) => {
   return (
-    <View>
-      <Text>Button</Text>
+    <View style={styles.btnContainer}>
+      <Text style={styles.btnText}>{title}</Text>
     </View>
   )
 }
 
 export default Button
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  btnContainer: {
+    borderRadius: 6,
+    backgroundColor: "navy",
+    width: "50%",
+    marginHorizontal: "auto"
+  },
+  btnText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+    paddingVertical: 8,
+    textAlign: "center"
+  }
+})
