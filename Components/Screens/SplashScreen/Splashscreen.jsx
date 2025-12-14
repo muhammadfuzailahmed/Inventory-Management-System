77b8af3777d231381aa4f6e7af348de9c0231197
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-14 18:38:49 +0500

    Add loader and visible functionality to splash screen

--- Components/Screens/SplashScreen/Splashscreen.jsx
+++ Components/Screens/SplashScreen/Splashscreen.jsx
@@ -1,14 +1,25 @@
-import { Image, Modal, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, Modal, StyleSheet, Text, View, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import IMS_Logo from "../../Assets/IMS_Logo.png"
 
-const Splashscreen = () => {
+const Splashscreen = ({navigation}) => {
+  const [showModal, setShowModal]= useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(false)
+      navigation.navigate("login")
+    }, 3000)
+  }, [])
+
   return (
-    <Modal animationType='slide'>
+    <Modal visible={showModal} animationType='slide'>
     <View style={styles.container}>
       <Image style={styles.img} source={IMS_Logo} />
+      <Text style={styles.heading}>InventoryPro</Text>
+      <ActivityIndicator size='large'/>
     </View>
-    </Modal>
+      </Modal>
   )
 }
 
@@ -23,6 +34,12 @@ const styles = StyleSheet.create({
   },
   img: {
     width:"60%",
-    height: "30%"
+    height: "25%",
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "900",
+    color: "white",
+    marginBottom: 30
   }
 })
