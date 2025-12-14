d29a118afb50597a67dfbf625dd18a0a0662e587
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-14 21:33:30 +0500

    Set initial route to share data across the app

--- Components/Navigation/BotomTab/TabScreens.jsx
+++ Components/Navigation/BotomTab/TabScreens.jsx
@@ -1,11 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from "../../Screens/Dashboard/Dashboard"
+import ShowAllProducts from "../../Screens/Products/ShowAllProducts/ShowAllProducts"
+import ShowAllExpenses from "../../Screens/Expenses/ShowAllExpenses/ShowAllExpenses"
+import sellProduct from "../../Screens/SellProducts/SellProduct/SellProduct"
+import { useRoute } from '@react-navigation/native';
 
-const TabScreens = () => {
+const Tab = createBottomTabNavigator();
+
+const TabScreens = ({route}) => {
+  const {user} = route.params || {};
   return (
-    <View>
-      <Text>TabScreens</Text>
-    </View>
+    <Tab.Navigator screenOptions={{headerShown: false}}>
+      <Tab.Screen name='dashboard' component={Dashboard} initialParams={{user}}/>
+      <Tab.Screen name='inventory' component={ShowAllProducts} initialParams={{user}}/>
+      <Tab.Screen name='expenseList' component={ShowAllExpenses} initialParams={{user}}/>
+      <Tab.Screen name='sellProduct' component={sellProduct} initialParams={{user}}/>
+    </Tab.Navigator>
   )
 }
 
