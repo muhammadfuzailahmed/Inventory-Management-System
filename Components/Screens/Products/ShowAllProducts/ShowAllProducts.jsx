cfb64897a0fbac894b60ba21989e6b2cebdf82bb
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-18 17:17:45 +0500

    Add AddProduct button

--- Components/Screens/Products/ShowAllProducts/ShowAllProducts.jsx
+++ Components/Screens/Products/ShowAllProducts/ShowAllProducts.jsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Alert, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ShowAllProducts = () => {
+const ShowAllProducts = ({navigation}) => {
+  
+  const handleAddProductBtn = () => {
+    navigation.navigate("AddProduct");
+  }
+  
   return (
     <View>
-      <Text>ShowAllProducts</Text>
+      <View style={styles.addProductBtnContainer}>
+      <Text onPress={handleAddProductBtn} style={styles.addProductBtn}>btn</Text>
+      </View>
     </View>
   )
 }
 
 export default ShowAllProducts
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  addProductBtnContainer: {
+    backgroundColor: "navy",
+    position: "absolute",
+    top: 90,
+    right: 15,
+    width: 100,
+    height: 50
+  },
+  addProductBtn: {
+    color: "#fff"
+  }
+})
