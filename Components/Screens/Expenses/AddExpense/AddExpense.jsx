b72cce63a9e5aed50eb0d1ab8b29ce7898ab2d5c
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-20 01:47:55 +0500

    Add UI and functionality to Add expense page

--- Components/Screens/Expenses/AddExpense/AddExpense.jsx
+++ Components/Screens/Expenses/AddExpense/AddExpense.jsx
@@ -1,14 +1,136 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import Button from '../../../UI/Button/Button';
+import Toast from 'react-native-toast-message';
+import axios from 'axios';
 
-const AddExpense = () => {
+const AddExpense = ({navigation, route}) => {
+  const {user} = route.params || {};
+  const [title, setTile] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+  // const [date, setDate] = useState("");
+
+  const handleCancelBtn = () => {
+    navigation.navigate("bottomTabs", {user});
+  }
+
+  const handleAddExpenseBtn = async () => {
+    if (!title || !category || !amount) {
+      Toast.show({
+        type: "error",
+        text1: "Fill all required fields"
+      })
+      return;
+    };
+    let id = user.id;
+    let reponse = await axios.post("http://192.168.100.99:5000/addExpense", {
+      title,
+      category,
+      amount,
+      id
+    }).then(() => {
+      Toast.show({
+        type: "success",
+        text1: "Expense Added Successfully!"
+      });
+
+      setTile("");
+      setCategory("");
+      setAmount("");
+      }).catch((error) => {
+      if (error.response) {
+        if (error.reponse.status = 404) {
+          Toast.show({
+            type: "error",
+            text1: "User not found"
+          });
+        } else if (error.response.status = 401) {
+          Toast.show({
+            type: "error",
+            text1: "Can't add expense!"
+          })
+        }
+      }
+    })
+
+  }
+
+
   return (
-    <View>
-      <Text>AddExpense</Text>
+    <View style={styles.container}>
+      <Button onPress={handleCancelBtn} title="Cancel"/>
+      <Text style={styles.expense}>Add Expense</Text>
+      <View style={styles.horizontalBar}></View>
+      <View style={styles.addExpenseForm}>
+        <TextInput value={title} onChangeText={(e) => setTile(e)} style={styles.input} placeholder='Title' />
+        <TextInput value={category} onChangeText={(e) => setCategory(e)} style={styles.input} placeholder='Category' />
+        <TextInput value={amount} onChangeText={(e) => setAmount(e)} style={styles.input} placeholder='Amount' />
+        {/* <TextInput value={date} onChangeText={(e) => setDate(e)} style={styles.input} placeholder='Data' /> */}
+      </View>
+      <Button onPress={handleAddExpenseBtn} title="Add Expense" />
     </View>
   )
 }
 
 export default AddExpense
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  expense: {
+    textAlign: "center",
+    color: '#000',
+    fontSize: 32,
+    fontWeight: 'bold'
+  },
+  horizontalBar: {
+    width: "50%",
+    backgroundColor: "navy",
+    height: 5,
+    borderRadius: 25
+  },
+  addExpenseForm: {
+    marginTop: 15,
+    width: "90%",
+    marginHorizontal: "auto"
+  },
+  input: {
+    width: "100%",
+    borderWidth: 2,
+    marginBottom: 12,
+    borderRadius: 8,
+    paddingHorizontal: 8
+  },
+  flexInput: {
+    width: "45%",
+    borderWidth: 2,
+    marginBottom: 12,
+    borderRadius: 8,
+    paddingHorizontal: 8
+  },
+  formFlex: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  productbutton: {
+    width: 350,
+    marginLeft: 20,
+    marginTop: 20,
+    borderRadius: 10,
+    height: 50,
+    fontSize: 30,
+    backgroundColor: '#005bd1ff',
+    paddingTop: 7,
+    paddingLeft: 90
+  },
+  saveexptext: {
+
+    fontSize: 24,
+    color: '#ffffffff'
+  }
+})
