dc169dbd9cdba0d839b43c4fa56510117a53efcd
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-20 01:48:30 +0500

    Add UI and functionality to Show All Expenses page

--- Components/Screens/Expenses/ShowAllExpenses/ShowAllExpenses.jsx
+++ Components/Screens/Expenses/ShowAllExpenses/ShowAllExpenses.jsx
@@ -1,14 +1,155 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, {useCallback, useState} from 'react'
+import Button from '../../../UI/Button/Button'
+import { useFocusEffect } from '@react-navigation/native'
+import { ActivityIndicator } from 'react-native'
 
-const ShowAllExpenses = () => {
-  return (
-    <View>
-      <Text>ShowAllExpenses</Text>
+const ShowAllExpenses = ({navigation, route}) => {
+  const {user} = route.params || {};
+
+  const [expenses, setExpenses] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  function handleAddExpenseBtn() {
+    navigation.navigate("AddExpense", { user })
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      const fetchProducts = async () => {
+        try {
+          const res = await fetch(
+            `http://192.168.100.99:5000/expenses/${user.id}`
+          );
+          setLoader(false);
+  
+          if (!res.ok) {
+            const error = await res.text();
+            console.log("Backend error:", error);
+            return;
+          }
+  
+          const data = await res.json();
+          setExpenses(data);
+  
+        } catch (error) {
+          console.log("Fetch error:", error);
+        }
+      };
+  
+      fetchProducts();
+    }, [])
+  );
+  
+
+  return loader ? ( 
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size='large'/>
+      </View> ):
+      (
+        <View>
+      <View style={styles.addProductBtnContainer}>
+      <Button onPress={handleAddExpenseBtn} title="Add Expense"/>
+      </View>
+      <View style={styles.expenseContainer}>
+            {expenses.length > 0 ? 
+          <FlatList 
+              data={expenses}
+              keyExtractor={(e) => e._id.toString()}
+              renderItem={({item}) => {
+                return (
+                  <View style={styles.expenseCard}>
+                    <View style={styles.flex}>
+                      <View>
+                      <Text style={styles.expenseTitle}>{item.expenseTitle}</Text>
+                      <View style={styles.cardFlex}>
+                      <Text style={styles.expenseCategory}>{item.expenseCategory}</Text>
+                      <Text>•</Text>
+                      <Text style={styles.expenseDate}>{new Date(item.createdAt).toLocaleDateString("en-GB")}</Text>
+                      </View>
+                      </View>
+                    <View>
+                      <Text style={styles.expenseAmount}>Rs.{item.expenseAmount}</Text>
+                    </View>
+                    </View>
+                    
+                </View>
+                )
+              }}
+              />: 
+              <Text>No expenses found!</Text>  
+          }  
+            </View>
     </View>
-  )
+      )
 }
 
 export default ShowAllExpenses
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  addProductBtnContainer: {
+    position: "absolute",
+    top: 40,
+    right: -55,
+    width: "70%"
+  },
+  expenseContainer: {
+    width: "95%",
+    marginHorizontal: "auto",
+    marginTop: 110
+  },
+  // input: {
+  //   borderWidth: 2,
+  //   borderColor: "black",
+  //   borderRadius: 6,
+  //   paddingLeft: 8,
+  //   width: "100%"
+  // },
+  // btn: {
+  //   position: "absolute",
+  //   right: -25,
+  //   width: 100
+  // },
+  expenseCard: {
+  backgroundColor: "#fff",
+  padding: 14,
+  marginVertical: 8,
+  borderRadius: 10,
+  elevation: 5,
+},
+flex: {
+  flexDirection: "row",
+  alignItems: "center",
+  justifyContent: "space-between",
+  marginHorizontal: 10
+},
+cardFlex: {
+  flexDirection: "row",
+  alignItems: "center",
+  gap: 5
+},
+expenseTitle: {
+  fontWeight: "800",
+  fontSize: 18
+},
+expenseCategory: {
+  fontWeight: "bold",
+  fontSize: 15,
+  color: "gray"
+},
+expenseAmount: {
+  fontSize: 15,
+  fontSize: 17,
+  fontWeight: "bold"
+},
+expenseDate: {
+  fontWeight: "bold",
+  fontSize: 15,
+  color: "gray"
+}
+})
