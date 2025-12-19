525d6ce15abc8c93053037af8ffa8810d3f60742
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-20 01:50:36 +0500

    Add search button and loader on show all products screen

--- Components/Screens/Products/ShowAllProducts/ShowAllProducts.jsx
+++ Components/Screens/Products/ShowAllProducts/ShowAllProducts.jsx
@@ -1,10 +1,13 @@
-import { Alert, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native'
+import {  FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native';
+import Button from '../../../UI/Button/Button';
+import { ActivityIndicator } from 'react-native';
 
 const ShowAllProducts = ({navigation, route}) => {
   const {user} = route.params || {};  
   const [items, setItems] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   const handleAddProductBtn = () => {
     navigation.navigate("AddProduct", {user});
@@ -17,7 +20,7 @@ useFocusEffect(
         const res = await fetch(
           `http://192.168.100.99:5000/products/${user.id}`
         );
-
+        setLoader(false);
         if (!res.ok) {
           const err = await res.text();
           console.log("Backend error:", err);
@@ -38,10 +41,21 @@ useFocusEffect(
 
 
 
-  return (
+  return loader ? (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large"/>
+    </View>
+  ) : 
+  (
     <View>
       <View style={styles.addProductBtnContainer}>
       <Text onPress={handleAddProductBtn} style={styles.addProductBtn}>Add Product</Text>
+      </View>
+      <View style={styles.searchProductContainer}>
+        <TextInput style={styles.input} placeholder='Search Product'/>
+        <View style={styles.btn}>
+        <Button title='🔎'/>
+        </View>
       </View>
       <View style={styles.productContainer}>
       {items.length > 0 ? 
@@ -73,6 +87,11 @@ useFocusEffect(
 export default ShowAllProducts
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
   addProductBtnContainer: {
     backgroundColor: "navy",
     borderRadius: 8,
@@ -89,10 +108,30 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold"
   },
+  searchProductContainer: {
+    width: "90%",
+    marginHorizontal: "auto",
+    marginTop: 110,
+    flexDirection: "row",
+    position: "relative"
+    
+  },
   productContainer: {
     width: "95%",
     marginHorizontal: "auto",
-    marginTop: 110
+    marginTop: 10
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: "black",
+    borderRadius: 6,
+    paddingLeft: 8,
+    width: "100%"
+  },
+  btn: {
+    position: "absolute",
+    right: -25,
+    width: 100
   },
   productCard: {
   backgroundColor: "#fff",
