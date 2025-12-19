f0ad334fbeef6c2e9fdd852818fe8732a587e28d
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-20 01:47:20 +0500

    Add dynamic data to dashboard page

--- Components/Screens/Dashboard/Dashboard.jsx
+++ Components/Screens/Dashboard/Dashboard.jsx
@@ -1,35 +1,84 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
+import { useFocusEffect } from '@react-navigation/native';
 
 const Dashboard = ({ route }) => {
   const { user } = route.params || {};
+  const [userData, setUserData] = useState(user);
+  const [totalValue, setTotalValue] = useState(0);
+  const [totalProfit, setTotalProfit] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+
+  useFocusEffect(
+  useCallback(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch(`http://192.168.100.99:5000/user/${user.id}`);
+        const data = await res.json();
+        setUserData(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (user?.id) fetchUser();
+  }, [user?.id])
+);
+
+  useEffect(() => {
+    calculateTotalValue();
+    calculateTotalProfit();
+    calculateTotalExpenses();
+  }, [userData])
+
+  const calculateTotalValue = () => {
+    const total = userData?.inventory?.reduce((sum, t) => {
+      return sum + (t.buyingPrice * t.quantity)
+    }, 0)
+    setTotalValue(total);
+  }
+
+  const calculateTotalProfit = () => {
+    const total = userData?.inventory?.reduce((sum, t) => {
+      return sum + ((t.sellingPrice - t.buyingPrice) * t.quantity)
+    }, 0)
+        setTotalProfit(total);
+  }
+
+  const calculateTotalExpenses = () => {
+    const total = userData?.expenseList?.reduce((sum, t) => {
+      return sum + t.expenseAmount;
+    }, 0)
+    setTotalExpenses(total);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.flex}>
         <View style={styles.logoContainer}>
-          <Text style={styles.logoName}>{user?.name[0]}</Text>
+          <Text style={styles.logoName}>{userData?.name[0]}</Text>
         </View>
-        <Text style={styles.greeting}>Hello, {user?.name}👋</Text>
+        <Text style={styles.greeting}>Hello, {userData?.name}👋</Text>
       </View>
       <Text style={styles.overviewText}>Here's current overview</Text>
       <View style={styles.row}>
         <View style={styles.card}>
           <Text style={styles.cardImg}>💲</Text>
           <Text style={styles.cardText}>Total Value</Text>
-          <Text style={styles.cardTotal}>$12,500</Text>
+            <Text style={styles.cardTotal}>Rs.{totalValue}</Text>
         </View>
         <View style={styles.card}>
           <Text style={styles.cardImg}>📈</Text>
-          <Text style={styles.cardText}>Total Value</Text>
-          <Text style={styles.cardTotal}>$12,500</Text>
+          <Text style={styles.cardText}>Total Profit</Text>
+          <Text style={styles.cardTotal}>Rs. {totalProfit}</Text>
         </View>
       </View>
 
       <View style={styles.row}>
         <View style={styles.card}>
           <Text style={styles.cardImg}>🛒</Text>
-          <Text style={styles.cardText}>Total Value</Text>
-          <Text style={styles.cardTotal}>$12,500</Text>
+          <Text style={styles.cardText}>Total Expenses</Text>
+          <Text style={styles.cardTotal}>Rs.{totalExpenses}</Text>
         </View>
         <View style={styles.card}>
           <Text style={styles.cardImg}>👜</Text>
