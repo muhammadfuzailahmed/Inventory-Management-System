051f8ad0730baaa84b1c5ea7c3a748873bb674d5
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-24 18:17:35 +0500

    Add icons in bottom navigation

--- Components/Navigation/BotomTab/TabScreens.jsx
+++ Components/Navigation/BotomTab/TabScreens.jsx
@@ -6,19 +6,52 @@ import ShowAllProducts from "../../Screens/Products/ShowAllProducts/ShowAllProdu
 import ShowAllExpenses from "../../Screens/Expenses/ShowAllExpenses/ShowAllExpenses"
 import sellProduct from "../../Screens/SellProducts/SellProduct/SellProduct"
 import Settings from "../../Screens/Settings/Settings"
-import { useRoute } from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const Tab = createBottomTabNavigator();
 
 const TabScreens = ({route}) => {
   const {user} = route.params || {};
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name='dashboard' component={Dashboard} initialParams={{user}}/>
-      <Tab.Screen name='inventory' component={ShowAllProducts} initialParams={{user}}/>
-      <Tab.Screen name='expenseList' component={ShowAllExpenses} initialParams={{user}}/>
-      <Tab.Screen name='sellProduct' component={sellProduct} initialParams={{user}}/>
-      <Tab.Screen name='settings' component={Settings} initialParams={{user}}/>
+    <Tab.Navigator screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: "navy",
+      tabBarInactiveTintColor: "black",
+      tabBarLabelStyle:{
+        fontSize: 13
+      },
+      tabBarStyle: {
+        height: 70
+      }
+      }}>
+      <Tab.Screen name='Dashboard' component={Dashboard} initialParams={{user}}
+      options={{tabBarIcon: ({color, size}) => (
+        <FontAwesome name="home" size={size} color={color} />
+      )}}
+      />
+      <Tab.Screen name='Inventory' component={ShowAllProducts} initialParams={{user}}
+      options={{tabBarIcon: ({color, size}) => (
+        <MaterialIcons name="inventory" size={size} color={color}/>
+      )}}
+      />
+      <Tab.Screen name='Expense' component={ShowAllExpenses} initialParams={{user}}
+      options={{tabBarIcon: ({color, size}) => (
+        <FontAwesome5 name="money-check-alt" size={size} color={color}/>
+      )}}
+      />
+      <Tab.Screen name='Sell' component={sellProduct} initialParams={{user}}
+      options={{tabBarIcon: ({color, size}) => (
+        <MaterialIcons name="sell" size={size} color={color}/>
+      )}}
+      />
+      <Tab.Screen name='Settings' component={Settings} initialParams={{user}}
+      options={{tabBarIcon: ({color, size}) => (
+        <Ionicons name="settings" size={size} color={color}/>
+      )}}
+      />
     </Tab.Navigator>
   )
 }
