2a41e90a6886c6065b740f3438bbc1d143efd34c
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-12-25 16:20:38 +0500

    Add functionality to pass props to invoice page

--- Components/Screens/SellProducts/SellProduct/SellProduct.jsx
+++ Components/Screens/SellProducts/SellProduct/SellProduct.jsx
@@ -1,33 +1,80 @@
 import React from 'react'
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert, Modal } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Alert, Modal, ActivityIndicator } from 'react-native';
 import Button from '../../../UI/Button/Button';
 import Toast from 'react-native-toast-message';
 import SellProductInvoice from "../SellProductInvoice/SellProductInvoice"
 
-const SellProduct = ({navigation}) => {
-
+const SellProduct = ({ navigation, route }) => {
+  const { user } = route.params || {};
   const [productName, setProductName] = useState("");
   const [quantity, setQuantity] = useState(0);
   const [modal, setModal] = useState(false);
+  const [items, setItems] = useState([]);
+  const [sellItemObj, setSellItemObj] = useState({});
+  const [dataFound, setDataFound] = useState(false);
+  const [completeProduct, setCompleteProduct] = useState({});
 
-  const handleCancelModal = () => {
+  function handleCancelModal() {
     setModal(false);
   }
 
   const handleSellItemBtn = () => {
-    if(!productName || !quantity) {
+    if (!productName || !quantity) {
       Toast.show({
         type: "error",
         text1: "Fill all required fields"
       });
       return;
     }
-    const sellItemObj = {
-      productName,
-      quantity
-    };
-    setModal(true)
+    fetchProductData();
+  }
+
+  const fetchProductData = async () => {
+    try {
+      const res = await fetch(
+        `http://192.168.100.99:5000/products/${user.id}`
+      );
+      if (!res.ok) {
+        const err = await res.text();
+        console.log("Backend error:", err);
+        return;
+      }
+
+      const data = await res.json();
+      setItems(data);
+
+    } catch (err) {
+      console.log("Fetch error:", err);
+    }
+
+    const product = items.find(
+      item => item.productName.toLowerCase() === productName.toLowerCase()
+    );
+    setCompleteProduct(product);
+    if(quantity > product.quantity) {
+      Toast.show({
+        type: "error",
+        text1: "Insufficient quantity"
+      });
+      return;
+    }
+    if (product) {
+      setSellItemObj({
+        productName,
+        quantity
+      })
+      setDataFound(true);
+      setModal(true);
+      setProductName("")
+      setQuantity("")
+    } else {
+      Toast.show({
+        type: "error",
+        text1: "Item not found"
+      })
+    }
+
   }
 
   return (
@@ -38,13 +85,13 @@ const SellProduct = ({navigation}) => {
         <TextInput value={productName} onChangeText={(e) => setProductName(e)} placeholder='Enter Product here' style={styles.input} />
         <TextInput value={quantity} onChangeText={(e) => setQuantity(e)} placeholder='Items to be sold' style={styles.input} />
       </View>
-        <Button onPress={handleSellItemBtn} title="Sell Item" />
-        <Modal visible={modal} animationType='slide'>
-          <View style={styles.cancelBtn}>
-            <Button onPress={handleCancelModal} title="Cancel"/>
-          </View>
-          <SellProductInvoice />
-        </Modal>
+      <Button onPress={handleSellItemBtn} title="Sell Item" />
+      <Modal visible={modal} animationType='slide'>
+        <View style={styles.cancelBtn}>
+          <Button onPress={handleCancelModal} title="Cancel" />
+        </View>
+        {dataFound && <SellProductInvoice product={sellItemObj} completeProduct={completeProduct} setModal={setModal} user = {user}/>}
+      </Modal>
     </View>
   )
 }
